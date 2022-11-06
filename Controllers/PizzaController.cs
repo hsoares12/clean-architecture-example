968ad37619a73e73e0d149364a57dcc20035739d
Hugues Soares <huguescarlos.soares@gmail.com>
2022-11-06 18:16:42 -0500

    wip

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,47 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+        PizzaService.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
+
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        // This code will save the pizza and return a result
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        // This code will update the pizza and return a result
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        // This code will delete the pizza and return a result
+    }
+}
